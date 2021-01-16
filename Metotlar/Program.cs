8094a8f8842f85f83ca4108e33aebad5f2d35cd9
Özcan Elmacı <ozcan.elmaci@ogr.deu.edu.tr>
2021-01-17 00:14:02 +0300

    Revert "Revert "Yazılım Geliştirme Kampı 3.ders sonu""
    This reverts commit 5e49ce53e9c0f110c3febd58a728711b6227f6f2.

---
+++ Metotlar/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır karpuzu";
+
+            Urun[] urunler = new Urun[] {urun1,urun2 };
+
+            //type-safe -- tip güvenli
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("-------------------");
+            }
+
+            Console.WriteLine("----------Metotlar--------------");
+
+            //instance - örnek
+            //encapsulation
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            //burası örnek gibi, birnevi olmaması gereken yazım şekli, çünkü stok adedi eklersek buna, urun classta değiştirmek daha kolay olacaktı
+            sepetManager.Ekle2("Armut", "Yeşil armut" , 12, 10);
+            sepetManager.Ekle2("Elma", "Yeşil elma" , 12, 9);
+            sepetManager.Ekle2("Karpuz", "Diyarbakır karpuzu", 12, 8);
+        }
+    }
+}
+
+
+//Do not repeat yourself - DRY - Clean  Code - Best Practice
